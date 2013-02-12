b0abb0fe424db192a6728213f2749cdb6b34b6e2
seb-koemei <seb@koemei.com>
2013-02-12 09:00:13 +0100

    Update php/API.php
    Fixed examples syntax

--- php/API.php
+++ php/API.php
@@ -337,8 +337,8 @@ if ($argc < 2)
 
 /*
 Examples:
-upload media:		API.php POST media text/xml test.mp3
-transcribe media:	API.php POST media text/xml media/{id}/transcribe
+upload media:		API.php POST text/xml media test.mp3
+transcribe media:	API.php POST text/xml media/{id}/transcribe
 transcription status:	API.php GET text/xml media/{id}/transcribe/{process-id}
 info about media:	API.php GET text/xml media/{id}
 */
